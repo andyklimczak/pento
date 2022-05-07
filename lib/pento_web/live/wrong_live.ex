21567c34f780ba4d1d973af352f77df01a682988
Andy Klimczak <andyklimczak@fastmail.com>
2022-05-07 14:58:40 -0400

    ch01

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,46 @@
+defmodule PentoWeb.WrongLive do
+  use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time(), number: Enum.random(1..10))}
+  end
+
+  def render(assigns) do
+    ~H"""
+      <h1>Your score: <%= @score %></h1>
+      <h2>
+        <%= @message %>
+        It's <%= @time %>
+      </h2>
+      <h2>
+        <%= for n <- 1..10 do %>
+          <a href="#" phx-click="guess" phx-value-number={n}><%= n %></a>
+        <% end %>
+      </h2>
+    """
+  end
+
+  def handle_event("guess", %{"number" => guess} = data, socket) do
+    if String.to_integer(guess) == socket.assigns.number do
+      message = "Your guess: #{guess}. Correct!"
+      score = socket.assigns.score + 10
+      {
+        :noreply,
+        assign(socket, message: message, score: score, time: time(), number: Enum.random(1..10))
+      }
+    else
+      message = "Your guess: #{guess}. Wrong. Guess again. "
+      score = socket.assigns.score - 1
+      {
+        :noreply,
+        assign(socket, message: message, score: score, time: time())
+      }
+    end
+  end
+
+  def time() do
+    DateTime.utc_now
+    |> to_string
+  end
+
+end
