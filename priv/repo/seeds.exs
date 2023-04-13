06b2dab1930a3240f9f82510527e8ab5521b6937
Andy Klimczak <andyklimczak@fastmail.com>
2023-04-13 21:54:22 +0100

    ch03

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,30 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Pento.Catalog
+
+products = [
+  %{
+    name: "Chess",
+    description: "The classic",
+    sku: 6_213_123,
+    unit_price: 10,
+  },
+  %{
+    name: "Tic Tac Toe",
+    description: "X and Os",
+    sku: 123_432,
+    unit_price: 12.00,
+  },
+  %{
+    name: "Table Tennis",
+    description: "Small tennis",
+    sku: 32_213_263,
+    unit_price: 12.00,
+  },
+]
+
+Enum.each(products, fn product ->
+  Catalog.create_product(product)
+end)
